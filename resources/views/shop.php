06a3f58ab4cf0717f00fc809d17b2423f49ec5ce
LuuGiaPhu <156999724+LuuGiaPhu@users.noreply.github.com>
2026-03-24 12:40:51 +0700

    Implement windowed pagination with ellipsis (1 2 ... 5 6 7 ... 25 26 >)

--- resources/views/shop.php
+++ resources/views/shop.php
@@ -1034,18 +1034,36 @@ body.cursor--img #bk-cursor-ring { width: 80px; height: 80px; border-color: rgba
 </ul>
 </div>
 <?php if ($products->lastPage() > 1): ?>
+<?php
+    $cur   = $products->currentPage();
+    $last  = $products->lastPage();
+    $delta = 2; // pages shown around current
+    $pages = [];
+    $prev  = null;
+    for ($i = 1; $i <= $last; $i++) {
+        if ($i <= 2 || $i >= $last - 1 || abs($i - $cur) <= $delta) {
+            if ($prev !== null && $i - $prev > 1) {
+                $pages[] = '...';
+            }
+            $pages[] = $i;
+            $prev = $i;
+        }
+    }
+?>
 <div class="mkd-woo-pagination-holder"><div class="mkd-woo-pagination-inner"><nav class="woocommerce-pagination">
 	<ul class='page-numbers'>
-	<?php if ($products->currentPage() > 1): ?>
+	<?php if ($cur > 1): ?>
 	<li><a class="prev page-numbers" href="<?php echo htmlspecialchars($products->previousPageUrl()); ?>">&larr;</a></li>
 	<?php endif; ?>
-	<?php for ($i = 1; $i <= $products->lastPage(); $i++): ?>
-	<?php if ($i == $products->currentPage()): ?>
-	<li><span aria-current="page" class="page-numbers current"><?php echo $i; ?></span></li>
+	<?php foreach ($pages as $p): ?>
+	<?php if ($p === '...'): ?>
+	<li><span class="page-numbers dots">&hellip;</span></li>
+	<?php elseif ($p == $cur): ?>
+	<li><span aria-current="page" class="page-numbers current"><?php echo $p; ?></span></li>
 	<?php else: ?>
-	<li><a class="page-numbers" href="<?php echo htmlspecialchars($products->url($i)); ?>"><?php echo $i; ?></a></li>
+	<li><a class="page-numbers" href="<?php echo htmlspecialchars($products->url($p)); ?>"><?php echo $p; ?></a></li>
 	<?php endif; ?>
-	<?php endfor; ?>
+	<?php endforeach; ?>
 	<?php if ($products->hasMorePages()): ?>
 	<li><a class="next page-numbers" href="<?php echo htmlspecialchars($products->nextPageUrl()); ?>">&rarr;</a></li>
 	<?php endif; ?>
